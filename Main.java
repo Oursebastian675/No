8a987fb068d495f57d2a76593776dfc1f8986a7d
Oursebastian675 <163418824+Oursebastian675@users.noreply.github.com>
2024-05-14 08:47:22 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("\n--- Calculadora de Nomina ---");
         Scanner src = new Scanner(System.in);
 
         // Solicitar datos del empleado
